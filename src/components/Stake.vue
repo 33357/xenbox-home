4e15307f57affae8fe335b61b5beccfe3436ec51
33357 <1043898730@qq.com>
2022-10-27 10:14:54 +0100

    0.0.1

---
+++ src/components/Stake.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <el-card class="box-card">
+        <template #header>
+          <div class="card-header">
+            <span>Card name</span>
+            <el-button class="button" text>Operation button</el-button>
+          </div>
+        </template>
+        <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+      </el-card>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  </script>
+  
+  <style>
+  .card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .text {
+    font-size: 14px;
+  }
+  
+  .item {
+    margin-bottom: 18px;
+  }
+  
+  .box-card {
+    width: 480px;
+  }
+  </style>
